47fdaa60c07ab3b604f0fba6a405d6046d27c2c9
aantu <jibonoshomapto3@gmail.com>
2021-11-19 16:34:41 -0800

    repo commit

---
+++ sctipt.js
@@ -0,0 +1,13 @@
+
+$(".owl-carousel").owlCarousel({
+  items: 3,
+  loop: true,
+  margin: 25,
+  nav: false,
+  
+});
+$('.product-list').masonry({
+  // // options
+  // itemSelector: '.grid-item',
+  // columnWidth: 200
+});
